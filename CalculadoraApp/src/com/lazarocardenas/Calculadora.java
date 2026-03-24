a8740cb18f1bfcb8dd2eaef7c099d2420f8e0814
Tarde <Tarde@DAW111>
2026-03-24 16:56:08 +0100

    Paso 10 corregido

--- CalculadoraApp/src/com/lazarocardenas/Calculadora.java
+++ CalculadoraApp/src/com/lazarocardenas/Calculadora.java
@@ -10,7 +10,6 @@ public class Calculadora {
 		 System.out.println("--- CALCULADORA DEL EQUIPO A y B ---");
 		 System.out.println("Suma: " + calc.sumar(10, 5));
 		 
-		 carolina asfdiasfdnjkfds
 		 
 	 }
 }
